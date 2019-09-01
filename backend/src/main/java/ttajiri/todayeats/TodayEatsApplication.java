cab268abcba5a40a2dcbcd0d97dfca93f20a25d5
t-tajiri <swallow.ava@gmail.com>
2019-09-01 21:31:17 +0900

    [WIP] scaffold backend project

---
+++ backend/src/main/java/ttajiri/todayeats/TodayEatsApplication.java
@@ -0,0 +1,13 @@
+package ttajiri.todayeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayEatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayEatsApplication.class, args);
+    }
+
+}
