fe47e65dc14ab33f4792e5126be4512e1c50df7c
t-tajiri <swallow.ava@gmail.com>
2019-09-02 21:07:01 +0900

    update options, deleting unnecessary options and update gradle config

--- test/src/test/java/ttajiri/todayeats/AcceptanceTest.java
+++ test/src/test/java/ttajiri/todayeats/AcceptanceTest.java
@@ -4,6 +4,5 @@ import io.cucumber.junit.*;
 import org.junit.runner.*;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = "pretty", features = "src/test/resources/features")
 public class AcceptanceTest {
 }
